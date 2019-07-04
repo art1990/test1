3706252d053c5c6e960778c2b4439a5556814c8f
Artem <gorbacartem90@gmail.com>
2019-07-04 23:43:15 +0300

    extension process

--- scripts/app.js
+++ scripts/app.js
@@ -2,62 +2,60 @@ const numberColumn = () => document.querySelector(".table-row").querySelectorAll
 const numberRow = () => document.querySelectorAll(".table-row").length;
 const tableColumn = (arg) => document.getElementsByClassName(arg);
 const tableRow = (arg) => document.getElementById(arg);
-let x, y;
+const btnRColumn = document.getElementsByClassName("btn-remove-column")[0];
+const btnRemoveRow = document.getElementsByClassName("btn-remove-row")[0];
+let x, y, movX, movY;
 
 
+const myAddListener = (div) => {
+    div.addEventListener('mouseover', (e) => {
+        x = e.target;
+        y = x.parentNode;
+        movX = event.clientX;
+        movY = event.clientY;
+        btnRColumn.style.left = (x.offsetLeft).toString() + "px";
+        btnRemoveRow.style.top = (y.offsetTop).toString() + "px";
+    });
+};
 
 const createTable = (col=4, row=4) => {
-
     for (let i = 0; i < col; i++){
         let cell = document.createElement('td');
         cell.className = "cell";
         cell.id = "column" + (numberColumn() + 1);
-        cell.addEventListener('mouseover', function(e) {
-            x = e.target;
-            y = x.parentNode;
-        });
+        myAddListener(cell);
         tableColumn("table-row")[0].appendChild(cell)
     }
 
 
-
-
     for (let j = 1; j < row; j++){
         let row = document.getElementsByClassName("table-row")[0].cloneNode(true);
-        row.addEventListener('mouseover', function(e) {
-            x = e.target;
-            y = x.parentNode;
-        });
+        myAddListener(row);
         row.id = "row" + (numberRow() + 1);
         tableRow("table").appendChild(row)
     }
 };
 
-createTable();
-
-
-
+createTable()
 
 const addRow = () => {
     createTable(0, 2)
 };
 
-
-
 const addColumn = () => {
-    let numberColumnCurrent = numberColumn()+1
+    let numberColumnCurrent = numberColumn()+1;
     const arr = document.querySelectorAll(".table-row");
     for (let i = 0; i < arr.length; i++) {
         let cell = document.createElement('td');
         cell.className = "cell";
         cell.id ="column" + numberColumnCurrent.toString();
-        console.log(cell.id)
         arr[i].appendChild(cell)
     }
 };
 
 
 const removeRow = (y) => y.parentNode.removeChild(y);
+
 const removeColumn = (x) => {
     x = "#" + x.id;
     let arr = document.querySelectorAll(x);
@@ -66,3 +64,7 @@ const removeColumn = (x) => {
     }
 
 };
+
+
+btnRColumn.addEventListener("mouseover", () => btnRemoveRow.classList.toggle("btn-remove-row-hover"));
+btnRColumn.addEventListener("mouseout", () => btnRemoveRow.classList.toggle("btn-remove-row-hover"));
